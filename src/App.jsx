34f720dee2fe31baf2308d49fa7794131b2d04ea
Smrutirekha Behera <64762527+Smrutirekha414@users.noreply.github.com>
2023-08-30 12:30:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import { SearchBar } from './components/SearchBar'
+import { SearchResultList } from './components/SearchResultList';
+
+function App() {
+  const [result, setResult] = useState([]);
+
+  //console.log(result);
+
+  return (
+    <>
+      <div className="App">
+        <div className="search-bar-container">
+          <SearchBar setResult={setResult}/>
+          <SearchResultList result={result}/>
+        </div>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
